b609a29e88fe196b003529d133233ead919a3771
Ross Patterson <me@rpatterson.net>
2009-01-09 02:48:38 +0000

    * Add proper type names

--- collective/formcriteria/criteria/simplestring.py
+++ collective/formcriteria/criteria/simplestring.py
@@ -7,6 +7,8 @@ class SimpleStringFormCriterion(
     common.FormCriterion, simplestring.ATSimpleStringCriterion):
     """A simple string form criterion"""
 
+    meta_type      = 'SimpleStringFormCriterion'
+    archetype_name = 'Simple String Form Criterion'
     shortDesc = 'Form Text'
 
 criteria.registerCriterion(
