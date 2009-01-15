abf5eeb67bf2439e67fbd755364cd693c9a4ed0e
Ross Patterson <me@rpatterson.net>
2009-01-15 20:35:11 +0000

    * Move some TAL expressions into the view class

--- collective/formcriteria/form.py
+++ collective/formcriteria/form.py
@@ -11,3 +11,36 @@ class SearchFormView(object):
         return [
             criterion for criterion in self.context.listCriteria()
             if interfaces.IFormCriterion.providedBy(criterion)]
+
+    @view.memoize
+    def criteriaFields(self):
+        results = []
+        for criterion in self.formCriteria():
+            crit_id = criterion.getId()
+            field = criterion.Field()
+            index = self.context.portal_atct.getIndex(field)
+
+            schematas = criterion.Schemata()
+            fieldsets = [key for key in schematas.keys() if key != 'metadata']
+            default_fieldset = (
+                (not schematas or schematas.has_key('default'))
+                and 'default' or fieldsets[0])
+            fieldset = self.request.get('fieldset', default_fieldset)
+
+            result = {
+                'id': crit_id,
+                'field': field,
+                'friendlyName': index.friendlyName or index.index,
+                'description': index.description,
+                'fields': schematas[fieldset].fields(),
+                'widget': criterion.widget,
+                }
+
+            results.append(result)
+        return results
+
+    def fields(self):
+        results = []
+        for criterion in self.criteriaFields():
+            results.extend(criterion['fields'])
+        return results
