f28d0b4b464c2d6429c67b18198fbd2d6f92b70b
Ross Patterson <me@rpatterson.net>
2009-08-22 03:35:25 +0000

    Checkpoint progress on KSS table sorting and selection.  Table renders header and footer properly and the KSS doesn't raise an error, but it's not working yet.

--- src/collective/formcriteria/foldercontents/form.py
+++ src/collective/formcriteria/foldercontents/form.py
@@ -4,7 +4,8 @@ from zope.component import getMultiAdapter
 from zope.app import pagetemplate
 from zope.i18n import translate
 
-import Acquisition
+from Acquisition import aq_base
+from Acquisition import aq_inner
 
 from Products.CMFCore.utils import getToolByName
 
@@ -37,6 +38,27 @@ class Table(tableview.Table):
         """Use the collection batch"""
         return self._batch
 
+    @property
+    def islastpage(self):
+        return self.batch.numpages == self.batch.pagenumber
+
+    @property
+    def items_on_page(self):
+        if self.islastpage:
+            remainder = self.batch.length % self.batch.size
+            if remainder == 0:
+                return self.batch.size
+            else:
+                return remainder
+        else:
+            return self.batch.pagesize
+
+    @property
+    def items_not_on_page(self):
+        items_on_page = list(self.items)
+        return [item for item in self.items if item not in
+                items_on_page]
+
 class FolderContentsTable(foldercontents.FolderContentsTable):
     """Use a table template which obeys the columns fields"""                
 
@@ -47,8 +69,8 @@ class FolderContentsTable(foldercontents.FolderContentsTable):
         """Use a table template which obeys the columns fields"""
         self.context = context
         self.request = request
-        self.contentFilter = contentFilter
-
+        self.contentFilter = request
+        
         column_vocab = context.Vocabulary('customViewFields')[0]
         link_columns = context.getField(
             'customViewLinks').getAccessor(context)()
@@ -68,14 +90,14 @@ class FolderContentsTable(foldercontents.FolderContentsTable):
     @instance.memoize
     def batch(self):
         """Let the collection batch the results"""
-        context = Acquisition.aq_inner(self.context)
+        context = aq_inner(self.context)
         return context.queryCatalog(self.contentFilter, batch=True)
 
     @property
     @instance.memoize
     def items(self):
         """Use the item brains"""
-        context = Acquisition.aq_inner(self.context)
+        context = aq_inner(self.context)
         plone_utils = getToolByName(context, 'plone_utils')
         plone_view = getMultiAdapter((context, self.request), name=u'plone')
         portal_workflow = getToolByName(context, 'portal_workflow')
@@ -152,7 +174,7 @@ class FolderContentsTable(foldercontents.FolderContentsTable):
     @property
     def buttons(self):
         buttons = []
-        context = Acquisition.aq_inner(self.context)
+        context = aq_inner(self.context)
         portal_actions = getToolByName(context, 'portal_actions')
         button_actions = portal_actions.listActionInfos(
             object=context, categories=('folder_topic_buttons', ))
@@ -178,6 +200,8 @@ class FolderContentsView(foldercontents.FolderContentsView):
     
     def contents_table(self):
         """Use the request as the contentFilter"""
-        table = FolderContentsTable(
-            self.context, self.request, contentFilter=self.request)
+        table = FolderContentsTable(self.context, self.request)
         return table.render()
+
+class FolderContentsKSSView(foldercontents.FolderContentsKSSView):
+    table = FolderContentsTable
