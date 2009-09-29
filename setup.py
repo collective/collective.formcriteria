bfebccef3b8fceed3ded54ca0f926474d8ba3eef
Ross Patterson <me@rpatterson.net>
2009-09-29 06:39:35 +0000

    Next version is going to be backwards incompatible so should be a major version.  The query, sort, and column containers will be the major change.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1.2'
+version = '2.0dev'
 
 tests_require = ['collective.testcaselayer',
                  'collective.monkeypatcher']
