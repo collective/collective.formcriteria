703fe418553a1540264dd53edfdb562fc9bc2117
Ross Patterson <me@rpatterson.net>
2010-03-05 21:48:49 +0000

    Use plone:portletRenderer to register layer specific portlets instead of using overrides

--- src/collective/formcriteria/portlet/listing.py
+++
@@ -1,8 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-from plone.portlet.collection import collection
-
-class Renderer(collection.Renderer):
-    """Use the richer template"""
-
-    render = ViewPageTemplateFile('listing.pt')
