7d16bd839837a30cc6ef5f1b4b27343864471893
Ross Patterson <me@rpatterson.net>
2011-01-11 21:12:01 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0a4'
+version = '2.0a5'
 
 tests_require = ['collective.testcaselayer>=1.3',
                  'collective.monkeypatcher',
