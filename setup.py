48f3ad49b7d004cc4d3e9ec05da5b3dd766ed342
Mirco Angelini <mirco.angelini@redturtle.it>
2012-01-04 09:11:41 +0000

    rilascio versione

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1.0-RT-BRANCH'
+version = '1.1.0-RT-BRANCH-02'
 
 tests_require = ['collective.testcaselayer',
                  'collective.monkeypatcher']
